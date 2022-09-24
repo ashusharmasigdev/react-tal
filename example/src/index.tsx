fdac7a6abcff0ab753e8e2dbd98011f041455760
ashusharmasigdev <ashusharma.sigmadev@gmail.com>
2022-09-25 02:31:47 +0530

    workflow

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,15 +4,15 @@ import { MyCounter } from 'react-tal'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <div>
-      <h2>Default counter</h2>
-      <MyCounter />
-    </div>
-    <hr />
-    <div>
-      <h2>Counter with predefined value</h2>
-      <MyCounter value={5} />
-    </div>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <div>
+            <h2>Default counter</h2>
+            <MyCounter />
+        </div>
+        <hr />
+        <div>
+            <h2>Counter with predefined value</h2>
+            <MyCounter value={5} />
+        </div>
+    </React.StrictMode>,
 )
